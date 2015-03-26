b0d2ce053fd41e1bab5aae1ec0edee2f5fa76209
Renegade334 <contact.caaeed4f@renegade334.me.uk>
2015-03-26 12:52:21 +0000

    Update license text in source headers and template defaults

--- src/ScaffoldCommand.php
+++ src/ScaffoldCommand.php
@@ -4,7 +4,7 @@
  *
  * @link https://github.com/phergie/phergie-irc-plugin-react-scaffold for the canonical source repository
  * @copyright Copyright (c) 2008-2015 Phergie Development Team (http://phergie.org)
- * @license http://phergie.org/license New BSD License
+ * @license http://phergie.org/license Simplified BSD License
  * @package Phergie\Irc\Plugin\React\Scaffold
  */
 
@@ -86,7 +86,7 @@ class ScaffoldCommand extends Command
             'author_name' => 'Phergie Development Team',
             'author_email' => 'team@phergie.org',
             'author_url' => 'http://phergie.org',
-            'license_name' => 'New BSD License',
+            'license_name' => 'Simplified BSD License',
             'license_url' => 'http://phergie.org/license',
             'license_value' => 'BSD-2-Clause',
         ];
